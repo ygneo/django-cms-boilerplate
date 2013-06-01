322836cb54fb6a821fc0b31db7bc292a8fd875f3
ygneo <antonio.barcia@gmail.com>
2013-06-02 01:44:12 +0200

    Simplifying settings and urls

--- urls.py
+++ urls.py
@@ -7,8 +7,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'img/favicon.ico')),
-    url(r'^', include('cms.urls')),
 )
 
 if settings.DEBUG:
